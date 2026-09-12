a1bd06049091e6a00391750c7b3a34b0506b0fb4
Enrique Sanchez <esanchezvela@microsoft.com>
2026-09-12 19:12:53 +0000

    udpate sles12payg module

--- sles12pyag/variables.tf
+++ sles12pyag/variables.tf
@@ -14,7 +14,12 @@ variable "prefix" {
 variable "cidr_bits" {
   description = "Number of bits dedicated to the subnet mask"
   type        = string
-  default     = "2"
+  default     = "8"
+}
+
+variable "userpassword" {
+  description = "Window's admin password"
+  type        = string
 }
 
 variable "payg" {
@@ -34,14 +39,34 @@ variable "payg" {
   }))
   default = [
     {
-      pubip         = true,
-      name          = "sles12sap",
-      publisher     = "suse",
-      offer         = "sles-sap-12-sp5",
-      sku           = "gen2",
-      image_version = "latest"
-      custom_data   = "/dev/null"
-      encrypt       = false
+      pubip                = true,
+      name                 = "sles12",
+      publisher            = "suse",
+      offer                = "sles-sap-12-sp5",
+      sku                  = "gen2",
+      image_version        = "latest",
+      size                 = "Standard_D2s_v4",
+      custom_data          = "custom_data/sles12.yml",
     }
   ]
 }
+
+variable "byos" {
+  type = list(object({
+    pubip                = bool
+    name                 = string
+    publisher            = string
+    offer                = string
+    sku                  = string
+    image_version        = string
+    size                 = string
+    custom_data          = string
+    encrypt              = optional(bool, false)
+    network_acceleration = optional(bool, false)
+    nics                 = optional(string, "1")
+    nvme                 = optional(string, "SCSI")
+  }))
+  default = [
+  ]
+}
+
